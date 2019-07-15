8576343cb4c6c2986c15ceb23ebe71efef8d7b76
PanicSheep <dohofer86@gmail.com>
2019-07-15 21:35:57 +0200

    Extends the memory model mock up.

--- UnitTests/MemoryModel.cpp
+++ UnitTests/MemoryModel.cpp
@@ -1,6 +1,17 @@
 #include "pch.h"
 #include "MemoryModel.h"
 
+void MemoryModel::Memcpy(const std::vector<ByteArray>& source_vec, const void* source,
+	std::vector<ByteArray>& destination_vec, void* destination,
+	std::size_t count)
+{
+	auto src = std::find_if(source_vec.begin(), source_vec.end(),
+		[source](const auto& arr) { return reinterpret_cast<const void*>(arr.data()) == source; });
+	auto dst = std::find_if(destination_vec.begin(), destination_vec.end(),
+		[destination](const auto& arr) { return reinterpret_cast<const void*>(arr.data()) == destination; });
+	std::copy_n(src->begin(), count, dst->begin());
+}
+
 void MemoryModel::clear()
 {
 	host.clear();
@@ -20,7 +31,40 @@ void* MemoryModel::MallocHost(std::size_t size)
 
 void MemoryModel::FreeHost(void* p)
 {
-	auto it = std::remove_if(host.begin(), host.end(), 
-		[p](auto&& arr) { return reinterpret_cast<void*>(arr.data()) == p; });
+	const auto it = std::remove_if(host.begin(), host.end(),
+		[p](const auto& arr) { return reinterpret_cast<const void*>(arr.data()) == p; });
 	host.erase(it, host.end());
+}
+
+void* MemoryModel::MallocDevice(std::size_t size)
+{
+	device.emplace_back(ByteArray(size));
+	return reinterpret_cast<void*>(device.back().data());
+}
+
+void MemoryModel::FreeDevice(void* p)
+{
+	const auto it = std::remove_if(device.begin(), device.end(),
+		[p](auto&& arr) { return reinterpret_cast<void*>(arr.data()) == p; });
+	device.erase(it, device.end());
+}
+
+void MemoryModel::MemcpyHostToHost(const void* source, void* destination, std::size_t count)
+{
+	Memcpy(host, source, host, destination, count);
+}
+
+void MemoryModel::MemcpyHostToDevice(const void* source, void* destination, std::size_t count)
+{
+	Memcpy(host, source, device, destination, count);
+}
+
+void MemoryModel::MemcpyDeviceToHost(const void* source, void* destination, std::size_t count)
+{
+	Memcpy(device, source, host, destination, count);
+}
+
+void MemoryModel::MemcpyDeviceToDevice(const void* source, void* destination, std::size_t count)
+{
+	Memcpy(device, source, device, destination, count);
 }
--- UnitTests/MemoryModel.h
+++ UnitTests/MemoryModel.h
@@ -6,6 +6,10 @@ class MemoryModel
 {
 	using ByteArray = std::vector<uint8_t>;
 	std::vector<ByteArray> host, device;
+
+	void Memcpy(const std::vector<ByteArray>& source_vec, const void* source,
+		std::vector<ByteArray>& destination_vec, void* destination,
+		std::size_t count);
 public:
 	MemoryModel() = default;
 	MemoryModel(const MemoryModel&) = delete;
@@ -18,5 +22,13 @@ public:
 	bool empty() const;
 
 	void* MallocHost(std::size_t size);
-	void FreeHost(void* p);
+	void FreeHost(void*);
+
+	void* MallocDevice(std::size_t size);
+	void FreeDevice(void*);
+
+	void MemcpyHostToHost(const void* source, void* destination, std::size_t count);
+	void MemcpyHostToDevice(const void* source, void* destination, std::size_t count);
+	void MemcpyDeviceToHost(const void* source, void* destination, std::size_t count);
+	void MemcpyDeviceToDevice(const void* source, void* destination, std::size_t count);
 };
